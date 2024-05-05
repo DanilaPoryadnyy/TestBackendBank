22a2c68a8af980fabeda7e94bd81a7794b8755e8
steepfire <steepfirety@gmail.com>
2024-05-05 16:41:29 +0300

    Подключил БД

--- src/main/java/com/example/testbackendbank/TestBackendBankApplication.java
+++ src/main/java/com/example/testbackendbank/TestBackendBankApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //Временно отключил JPA, т.к. нет БД
+@SpringBootApplication
 public class TestBackendBankApplication {
 
     public static void main(String[] args) {
